fa404e9c1cd4af224a983b3ee72c0e2b98b138da
Tobias Ortmayr <tortmayr@eclipsesource.com>
2019-05-15 10:58:10 +0200

    Add code from graphical-lsp/client/packages/sprotty-client

---
+++ src/base/di.config.ts
@@ -0,0 +1,34 @@
+/********************************************************************************
+ * Copyright (c) 2019 EclipseSource and others.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v. 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0.
+ *
+ * This Source Code may also be made available under the following Secondary
+ * Licenses when the conditions for such availability set forth in the Eclipse
+ * Public License v. 2.0 are satisfied: GNU General Public License, version 2
+ * with the GNU Classpath Exception which is available at
+ * https://www.gnu.org/software/classpath/license.html.
+ *
+ * SPDX-License-Identifier: EPL-2.0 OR GPL-2.0 WITH Classpath-exception-2.0
+ ********************************************************************************/
+import "../../css/glsp-sprotty.css";
+
+import { ContainerModule } from "inversify";
+import { configureCommand, Tool, TYPES } from "sprotty/lib";
+
+import { GLSP_TYPES } from "../types";
+import { FeedbackAwareUpdateModelCommand } from "./model/update-model-command";
+import { createToolFactory, ToolManagerActionHandler } from "./tool-manager/tool-manager-action-handler";
+
+const defaultGLSPModule = new ContainerModule((bind, unbind, isBound, rebind) => {
+    // Tool manager initialization ------------------------------------
+    bind(TYPES.IActionHandlerInitializer).to(ToolManagerActionHandler);
+    bind(GLSP_TYPES.IToolFactory).toFactory<Tool>((createToolFactory()));
+
+    // Model update initialization ------------------------------------
+    configureCommand({ bind, isBound }, FeedbackAwareUpdateModelCommand);
+});
+
+export default defaultGLSPModule;
